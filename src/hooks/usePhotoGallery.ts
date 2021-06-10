4e5074364396452ac9944f36f77e2d7d7571fcb2
Chiaki Obara <obara@ham.works>
2021-06-10 17:19:28 +0900

    usePhotoGallery書き直し

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -1,65 +1,74 @@
-import {useState, useEffect, FC} from "react";
-import { useCamera } from '@ionic/react-hooks/camera';
-import { useFilesystem } from '@ionic/react-hooks/filesystem';
-import { useStorage } from '@ionic/react-hooks/storage';
-import { isPlatform } from '@ionic/react';
-import { CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirectory } from "@capacitor/core";
+import { useState, useEffect } from "react";
+import { isPlatform } from "@ionic/react";
+import {
+  Camera,
+  CameraPhoto,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from "@capacitor/camera";
 import { Filesystem, Directory } from "@capacitor/filesystem";
+import { Capacitor } from "@capacitor/core";
 
-export type Photo = {
+export type UserPhoto = {
   filepath: string;
   webviewPath?: string;
-}
+};
 
 export const usePhotoGallery = () => {
-  const [photos, setPhotos] = useState<Photo[]>([]);
-  const fileName = `${new Date().getTime()}.jpg`;
-
-  const { getPhoto } = useCamera();
-
-  const savePicture = async (photo: CameraPhoto, fileName: string) :Promise<Photo> => {
-    const base64Data = await base64FromPath(photo.webPath!);
-    const savedFile = await Filesystem.writeFile({
-      path: fileName,
-      data: base64Data,
-      directory: Directory.Data
-    });
-
-    return {
-      filepath: fileName,
-      webviewPath: photo.webPath
-    }
-  }
+  const [photos, setPhotos] = useState<UserPhoto[]>([]);
 
   const takePhoto = async () => {
-    const cameraPhoto: CameraPhoto = await getPhoto({
+    const cameraPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100,
     });
 
-    const fileName = new Date().getTime() + '.jpeg';
-    const savedFileImage = await savePicture(cameraPhoto, fileName);
-    const newPhotos = [savedFileImage, ...photos];
+    const fileName = `${new Date().getTime()}.jpeg`;
+    const newPhotos: UserPhoto[] = [
+      {
+        filepath: fileName,
+        webviewPath: cameraPhoto.webPath,
+      },
+      ...photos,
+    ];
     setPhotos(newPhotos);
-  }
+  };
+
+  const savePicture = async (
+    photo: CameraPhoto,
+    fileName: string
+  ): Promise<Photo> => {
+    const base64Data = await base64FromPath(photo.webPath!);
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
+  };
 
   return { photos, takePhoto };
-}
+};
 
-export async function base64FromPath(path: string): Promise<string> {
+export const base64FromPath = async (path: string): Promise<string> => {
   const response = await fetch(path);
   const blob = await response.blob();
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.onerror = reject;
     reader.onload = () => {
-      if (typeof reader.result === 'string') {
+      if (typeof reader.result === "string") {
         resolve(reader.result);
       } else {
-        reject('method did not return a string')
+        reject("method did not return a string");
       }
     };
     reader.readAsDataURL(blob);
   });
-}
+};
